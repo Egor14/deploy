872437f30678144c60bc07a8dc53a335822d56e7
Egor14 <egorgrekhnev@mail.ru>
2019-03-31 16:25:08 +0300

    fix

--- lean2/settings.py
+++ lean2/settings.py
@@ -125,3 +125,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.10/howto/static-files/
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
